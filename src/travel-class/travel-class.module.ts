3e06614cc68336db26aa8223144b289c6eb1fe71
Isaac David <idbl64@outlook.com>
2025-05-17 23:19:58 -0500

    Delete not asked test files and prior work

--- src/travel-class/travel-class.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { TravelClassService } from './travel-class.service'
-import { TravelClassController } from './travel-class.controller'
-
-@Module({
-  controllers: [TravelClassController],
-  providers: [TravelClassService],
-})
-export class TravelClassModule {}
--- src/travel-voucher/travel-voucher.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { TravelVoucherService } from './travel-voucher.service'
-import { TravelVoucherController } from './travel-voucher.controller'
-
-@Module({
-  controllers: [TravelVoucherController],
-  providers: [TravelVoucherService],
-})
-export class TravelVoucherModule {}
